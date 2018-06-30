5ef1d46a79f38547ad971282bb2697f28487c2f5
Yuval Idan <Yuval@Merediths-MacBook-Air.local>
2018-06-30 18:09:41 -0400

    display with messages

--- client/src/components/DisplayResults.jsx
+++ client/src/components/DisplayResults.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
+const negativeMessages = [
+    'You might want to look into that.',
+    'That doesn\'t look too good.',
+    'You can do better than that!',
+    'Not super ideal.',
+    'As homogeneous as Fox News',
+];
+const positiveMessages = [
+    'Looks good!',
+    'Tumblr would approve',
+    'Nice!',
+    'Super woke.',
+    'We\'re proud of you.',
+    'Good job.',
+    'So diverse it could be the cast of Orange is the New Black',
+];
+
+function getRandom(arr) {
+    return arr[Math.floor(Math.random()*arr.length)];
+}
+
+function percentageAndText(per) {
+    const text = per > 60 ? getRandom(negativeMessages) : getRandom(positiveMessages);
+    return `${per}%. ${text}`;
+}
+
 const SubmissionForm = ({results}) => {
     if (!results || !results.numberOfPeople) {
         return null;
@@ -7,9 +33,9 @@ const SubmissionForm = ({results}) => {
     return (
         <div>
             <h4>Total people identified: {results.numberOfPeople}</h4>
-            <h4>Men: {results.percentMen}%</h4>
-            <h4>White: {results.percentWhite}%</h4>
-            <h4>Young: {results.percentYoung}%</h4>
+            <h4>Men: {percentageAndText(results.percentMen)}</h4>
+            <h4>White: {percentageAndText(results.percentWhite)}</h4>
+            <h4>Young: {percentageAndText(results.percentYoung)}</h4>
         </div>
     );
 };
