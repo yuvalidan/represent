ece3f1b35fe5e5859178d29168ab93c77c304b08
Yuval Idan <yuval.idan5@gmail.com>
2018-07-01 09:48:44 -0400

    more deploy attempts

--- server.js
+++ server.js
@@ -708,9 +708,10 @@ app.get('/api/hello', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/public')));
+  console.log('here!!')
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
   }
 
