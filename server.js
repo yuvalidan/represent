0f752e6ca397962db38525396f26c1c9b393b0d0
Yuval Idan <yuval.idan5@gmail.com>
2018-06-30 21:53:50 -0400

    fix routes

--- server.js
+++ server.js
@@ -707,4 +707,11 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/public')));
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
+    });
+  }
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
